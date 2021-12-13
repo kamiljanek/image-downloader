85ae0c96eeba6b281a25d91431f6faf0a5e3bb4b
kamiljanek <gttony@o2.pl>
2021-12-13 19:23:49 +0100

    create Methods, new Menu, create Class in new file

--- WeatherArchives/WeatherArchives/Program.cs
+++ WeatherArchives/WeatherArchives/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Text.RegularExpressions;
 using System.Net;
 using System.Text.RegularExpressions;
 
@@ -8,85 +7,24 @@ namespace WeatherArchives
 {
     class Program
     {
-        //public static string linkSecondPart; //why this should be here instead of 3 lines below????
+        
+            private static string linkFirstPart = "http://flymet.meteopress.cz/";
         static void Main(string[] args)
         {
-            var weatherPropertiesList = new List<WeatherProperties>() {
-                new WeatherProperties(1,"oblcX","allClouds"),
-                new WeatherProperties(2,"srzk","precipitation")
-            };
-            string title = "WEATHER ARCHIVES";
-            string pageAdress = "http://flymet.meteopress.cz/";
-            string enter = "";
-            string linkFirstPart = "http://flymet.meteopress.cz/";
+            
+            const string localArchivePath = "C:\\Users\\JoHaNek\\Desktop\\archiwum\\";
             string linkSecondPart = string.Empty;
-            string linkToday = "cr/";
-            string linkTomorrow = "crdl/";
-            const string linkDayAfterTomorrow = "crdl1/"; //what is the diference compare to prewious one???
-            string localArchivePath = @"C:\Users\JoHaNek\Desktop\archiwum\";
-            var element = (allClouds: "oblcX", precipitation: "srzk", temp: "t2m", wind10m: "vitrx", gusts: "vitry", wind850hPa: "vitra", wind800hPa: "vitrb", 
-                kindOfThermals: "drtr", convTempDeficit: "cudf", cumulusClouds: "cukh", climbSpeed: "cuvr"); //why I can't use string variable???
-      
-        
+     
+            
 
-        Console.SetCursorPosition((Console.WindowWidth - pageAdress.Length) / 2, Console.CursorTop); //centering text
-            Console.WriteLine(pageAdress);
-
-            Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop); //centering text
-            Console.WriteLine(title);
-
-            Console.WriteLine(enter);
-
-            Console.WriteLine("\tWrite day do you want to download?");
-
-            var chooseDay = "1 - for Today\r\n2 - for Tomorrow\r\n3 - for Day After Tomorrow";
-
-            Console.WriteLine(chooseDay);
-           
-            string day = Console.ReadLine();
-
-            if (day == "1")
-            {
-                
-                Console.WriteLine("You chose Today");
-                linkSecondPart = linkToday;
-            }
-            else if (day == "2")
-            {
-                Console.WriteLine("You chose Tomorrow");
-                linkSecondPart = linkTomorrow;
-            }
-            else if (day == "3")
-            {
-                Console.WriteLine("You chose Day After Tomorrow");
-                linkSecondPart = linkDayAfterTomorrow;        
-            }
-            else
-            {
-                Console.WriteLine("Wrong number! Choose number between 1, 2, 3"); 
-                //need to create exceptions for incorect number to string linkSecondPart
-            }
-
-            Console.WriteLine(linkSecondPart);
-            Console.WriteLine("\tWrite numbers which elements are you looking for");
-
-            foreach (WeatherProperties val in weatherPropertiesList)
-            {
-                Console.WriteLine($"{val.Id} - {val.Name}");
-            }
-
-            //var chooseElement = "1 - All Clouds\r\n2 - Precipitation\r\n3 - Temperature\r\n4 - Wind 10m\r\n5 - Gusts" +
-            //   "\r\n6 - Wind 850 hPa\r\n7 - Wind 800 hPa\r\n8 - Kind Of Thermals\r\n9 - Convective Temperature Deficit\r\n10 - Cumulus Clouds\r\n11 - Climb Speed";
-            //Console.WriteLine(chooseElement);
-            //do not forget to create wind,temp, rain etc CHOOSE!!!
-
-            string elementInput = Console.ReadLine();
-            string[] numberElementInputs = elementInput.Split(',');
-            foreach (var item in collection)
-            {
-
-            }
-            weatherPropertiesList.Find(weatherProperty=>weatherProperty.Id==numberElementInput).Code;
+            Title();
+            MainMenu();
+    
+ 
+            //string elementInput = Console.ReadLine();
+            //string[] numberElementInputs = elementInput.Split(',');
+            
+            //weatherPropertiesList.Find(ChoosingProperty=>ChoosingProperty.Id==numberElementInputs).Code;
             //string[] numberElementInputReplace = { numberElementInput,  };
 
 
@@ -96,11 +34,11 @@ namespace WeatherArchives
             Console.WriteLine("\te.g.   9,12,15,17");
             
             string hoursInput = Console.ReadLine();
+            string[] numbersHoursInput = hoursInput.Split(",");
+            //string[] numbersHoursInput = Regex.Split(hoursInput, @"\D+"); //create table
 
-            string[] numbersHoursInput = Regex.Split(hoursInput, @"\D+"); //create table
 
-
-            foreach (string value in numbersHoursInput) //loop
+            foreach (string value in numbersHoursInput) 
             {
                 if (!string.IsNullOrEmpty(value))
                 {
@@ -114,65 +52,168 @@ namespace WeatherArchives
                     Console.WriteLine(link);
                     WebClient webClient = new WebClient();
                     //webClient.DownloadFile(link, fileName);
-                    //Parachute parachute = new Parachute(7);
-                    //parachute.Open();
-                    
+                                                           
                 }
 
             }
          
         }
-        public class WeatherProperties
+        //public class ChoosingProperties
+        //{
+        //    public ChoosingProperties(int id, string code, string name)
+        //    {
+        //        Id = id;
+        //        Name = name;
+        //        Code = code;
+        //    }
+
+        //    public int Id { get; set; }
+        //    public string Name { get; set; }
+        //    public string Code { get; set; }
+           
+        //}
+         
+        static void Title()
         {
-            public WeatherProperties(int id, string code, string name)
+            string enter = "";
+            string title = "WEATHER ARCHIVES";
+            string pageAdress = "http://flymet.meteopress.cz/";
+            Console.SetCursorPosition((Console.WindowWidth - pageAdress.Length) / 2, Console.CursorTop); //centering text
+            Console.WriteLine(pageAdress);
+
+            Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop); //centering text
+            Console.WriteLine(title);
+            Console.WriteLine(enter);
+        }
+        static void ClearWindow()
+        {
+            Console.Clear();
+            Title();
+        }
+        static void MainMenu()
+        {
+            Console.WriteLine("1. Day?");
+            Console.WriteLine("2. Type of Data?");
+            Console.WriteLine("3. Time?");
+            Console.WriteLine("4. Start Download");
+
+            string opcionInput = Console.ReadLine();
+            if (opcionInput == "1")
             {
-                Id = id;
-                Name = name;
-                Code = code;
+                ClearWindow();
+                ChooseDayMenu();
+            }
+            else if (opcionInput == "2")
+            {
+                ClearWindow();
+                ChooseWeatherProperties();
+            }
+            else if (opcionInput == "3")
+            {
+                ClearWindow();
+                ChooseTime();
+            }
+            else if (opcionInput == "4")
+            {
+                ClearWindow();
+                StartDownload();
             }
 
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Code { get; set; }
-           
-        }
-       // public enum WeatherProperties
-       // { 
-          //  oblcX=1, 
-         //   srzk=2, 
-          //  t2m=3 
-      //  }
-            public enum Season
-        {
-            Spring,
-            Summer,
-            Autumn,
-            Winter
-        }
-        public class Parachute
-        {
-            public Parachute()
+            else
             {
+                ClearWindow();
+                Console.WriteLine("WRONG CHOOISE!!! TRY ONE MORE TIME");
+                MainMenu();
+            }
+        }
+        static void ChooseDayMenu()
+        {
+            Console.WriteLine("\tType day are you intrested in?");
+            var chooseDayList = new List<ChoosingProperties>() {
+                new ChoosingProperties(1,"cr/","Today"),
+                new ChoosingProperties(2,"crdl/","Tomorrow"),
+                new ChoosingProperties(3,"crdl1/","Day After Tomorrow")
+            };
+            foreach (ChoosingProperties val in chooseDayList)
+            {
+                Console.WriteLine($"{val.Id} - {val.Name}");
+            }
+            Console.WriteLine("e.g. 1,2");
 
-            }
-            public Parachute(int hexColour)
-            {
-                HexColour = hexColour;
-            }
-            public int HexColour { get; set; } = 0;
-            public bool Open()
-            {
-                return false;
-            }
-            public bool Open(bool properPool)
-            {
-                return properPool;
-            }
-            public bool Open(string logo)
-            {
-                return false;
-            }
+            string numbersDayInput = Console.ReadLine();
+            string[] numberDayInputs = numbersDayInput.Split(',');
+
+            // chooseDayList.Find(chooseDayList => chooseDayList.Id == numberDayInput).Code;
+            //string[] numberElementInputReplace = { numberElementInput,  };
            
         }
+       
+        static void ChooseWeatherProperties()
+        {
+            Console.WriteLine("\tWrite numbers which elements are you looking for");
+            var weatherPropertiesList = new List<ChoosingProperties>() {
+                new ChoosingProperties(1,"oblcX","All Clouds"),
+                new ChoosingProperties(2,"srzk","Precipitation"),
+                new ChoosingProperties(3,"t2m","Temperature"),
+                new ChoosingProperties(4,"vitrx","Wind 10m"),
+                new ChoosingProperties(5,"vitry","Gust"),
+                new ChoosingProperties(6,"vitra","Wind 850 hPa"),
+                new ChoosingProperties(7,"vitrb","Wind 800 hPa"),
+                new ChoosingProperties(8,"drtr","Kind of Thermals"),
+                new ChoosingProperties(9,"cudf","Convective Temperature Deficit"),
+                new ChoosingProperties(10,"cukh","Cumulus Clouds"),
+                new ChoosingProperties(11,"cuvr","Climb Speed")
+            };
+            foreach (ChoosingProperties val in weatherPropertiesList)
+            {
+                Console.WriteLine($"{val.Id} - {val.Name}");
+            }
+            Console.WriteLine("e.g. 1,2,5,8");
+
+            string numbersWeatherPropertiesInput = Console.ReadLine();
+            string[] numberWeatherPropetiesInputs = numbersWeatherPropertiesInput.Split(',');
+        }
+
+        static void ChooseTime()
+        {
+
+            Console.WriteLine("\tWhat time are you looking for?");
+            Console.WriteLine("\te.g.   9,12,15,17");
+
+            string numbersTimeInput = Console.ReadLine();
+            string[] numberTimeInputs = numbersTimeInput.Split(",");
+            
+
+        }
+        static void StartDownload()
+        {
+
+          
+        }
+        //public class Parachute
+        //{
+        //    public Parachute()
+        //    {
+
+        //    }
+        //    public Parachute(int hexColour)
+        //    {
+        //        HexColour = hexColour;
+        //    }
+        //    public int HexColour { get; set; } = 0;
+        //    public bool Open()
+        //    {
+        //        return false;
+        //    }
+        //    public bool Open(bool properPool)
+        //    {
+        //        return properPool;
+        //    }
+        //    public bool Open(string logo)
+        //    {
+        //        return false;
+        //    }
+
+        //}
     }
 }
