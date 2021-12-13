7d2d13f726544ae593f3d919215996fd56725ca8
kamiljanek <gttony@o2.pl>
2021-12-13 23:55:26 +0100

    create now Class

---
+++ WeatherArchives/WeatherArchives/ChoosingProperties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherArchives
+{
+    public class ChoosingProperties
+    {
+        //public ChoosingProperties(int id, string code, string name)
+        //{
+        //    Id = id;
+        //    Code = code;
+        //    Name = name;
+        //}
+
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+    }
+}
