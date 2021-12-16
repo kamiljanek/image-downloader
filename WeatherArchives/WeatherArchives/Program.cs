df740ad7a1b0b20fc8117ef54312a5bda70249b3
kamiljanek <gttony@o2.pl>
2021-12-16 01:45:33 +0100

    finish MainMenu, finish LinkGenerator,clean up code

--- WeatherArchives/WeatherArchives/Program.cs
+++ WeatherArchives/WeatherArchives/Program.cs
@@ -8,71 +8,21 @@ namespace WeatherArchives
     class Program
     {
         
-            private static string linkFirstPart = "http://flymet.meteopress.cz/";
         static void Main(string[] args)
         {
-            
-            const string localArchivePath = "C:\\Users\\JoHaNek\\Desktop\\archiwum\\";
-            string linkSecondPart = string.Empty;
 
-            ChooseDayMenu();
+            // const string localArchivePath = "C:\\Users\\JoHaNek\\Desktop\\archiwum\\";
 
             Title();
             MainMenu();
-    
- 
-            //string elementInput = Console.ReadLine();
-            //string[] numberElementInputs = elementInput.Split(',');
-            
-            //weatherPropertiesList.Find(ChoosingProperty=>ChoosingProperty.Id==numberElementInputs).Code;
-            //string[] numberElementInputReplace = { numberElementInput,  };
 
-
-
-
-            Console.WriteLine("\tWhich hours are you looking for?");
-            Console.WriteLine("\te.g.   9,12,15,17");
-            
-            string hoursInput = Console.ReadLine();
-            string[] numbersHoursInput = hoursInput.Split(",");
-            //string[] numbersHoursInput = Regex.Split(hoursInput, @"\D+"); //create table
-
-
-            foreach (string value in numbersHoursInput) 
-            {
-                if (!string.IsNullOrEmpty(value))
-                {
-                    int i = int.Parse(value);
-                    Console.WriteLine($"Number: {i}");
-                    string hour = i.ToString();
-                       
-                    string fileName = $"{localArchivePath}{hour}.png";
-                    Console.WriteLine(fileName);
-                    string link = linkFirstPart + linkSecondPart + hour + ".png";
-                    Console.WriteLine(link);
-                    WebClient webClient = new WebClient();
-                    //webClient.DownloadFile(link, fileName);
-                                                           
-                }
-
-            }
-         
         }
-        //public class ChoosingProperties
-        //{
-        //    public ChoosingProperties(int id, string code, string name)
-        //    {
-        //        Id = id;
-        //        Name = name;
-        //        Code = code;
-        //    }
 
-        //    public int Id { get; set; }
-        //    public string Name { get; set; }
-        //    public string Code { get; set; }
-           
-        //}
-         
+
+        static List<string> firstPartLinks = new List<string>();
+        static List<string> secondPartLinks = new List<string>();
+        static List<string> thirdPartLinks = new List<string>();
+
         static void Title()
         {
             string enter = "";
@@ -92,6 +42,8 @@ namespace WeatherArchives
         }
         static void MainMenu()
         {
+            Console.Clear();
+            Title();
             Console.WriteLine("1. Day?");
             Console.WriteLine("2. Type of Data?");
             Console.WriteLine("3. Time?");
@@ -101,21 +53,27 @@ namespace WeatherArchives
             if (opcionInput == "1")
             {
                 ClearWindow();
-                ChooseDayMenu();
+                firstPartLinks = GenerateFirstPartLink();
+                MainMenu();
             }
             else if (opcionInput == "2")
             {
                 ClearWindow();
-                ChooseWeatherProperties();
+                secondPartLinks = GenerateSecondPartLink();
+                MainMenu();
             }
             else if (opcionInput == "3")
             {
                 ClearWindow();
-                ChooseTime();
+                thirdPartLinks = GenerateThirdPartLink();
+                MainMenu();
             }
             else if (opcionInput == "4")
             {
                 ClearWindow();
+                GenerateAllProperties();
+                Confirmation();
+                GenerateCompleteLinks();
                 StartDownload();
             }
 
@@ -126,70 +84,108 @@ namespace WeatherArchives
                 MainMenu();
             }
         }
-        static void ChooseDayMenu()
+        static List<string> GenerateFirstPartLink()
         {
-            //Console.WriteLine("\tType day are you intrested in?");
-            //var chooseDayList = new List<ChoosingProperties>() {
-            //    new ChoosingProperties(1,"cr/","Today"),
-            //    new ChoosingProperties(2,"crdl/","Tomorrow"),
-            //    new ChoosingProperties(3,"crdl1/","Day After Tomorrow")
-            //};
-
-            List<ChoosingProperties> chooseDayList = new List<ChoosingProperties>();
-            chooseDayList.Add(new ChoosingProperties() { Id = 1, Code = "cr/", Name = "Today" });
-            chooseDayList.Add(new ChoosingProperties() { Id = 2, Code = "crdl/", Name = "Tomorrow" });
-            chooseDayList.Add(new ChoosingProperties() { Id = 3, Code = "crdl1/", Name = "Day After Tomorrow" });
+            Console.WriteLine("\tWrite numbers which days do you want to download:");
+            var chooseDayList = new List<ChoosingProperties>
+            {
+                 new ChoosingProperties("1", "cr/", "Today"),
+                 new ChoosingProperties("2","crdl/","Tomorrow"),
+                 new ChoosingProperties("3","crdl1/","Day After Tomorrow")
+            };
 
             foreach (ChoosingProperties val in chooseDayList)
             {
                 Console.WriteLine($"{val.Id} - {val.Name}");
             }
+            Console.WriteLine("\te.g. 1,3");
             string numbersDayInput = Console.ReadLine();
             string[] numberDayInputs = numbersDayInput.Split(',');
 
-            //Console.WriteLine(chooseDayList.Find(x => x.Id.Contains($"{numberDayInputs}")));
-            //foreach (ChoosingProperties val in chooseDayList)
-            //{
-            //    Console.WriteLine($"{val.Id} - {val.Name}");
-            //}
-            //Console.WriteLine("e.g. 1,2");
-
-            //string numbersDayInput = Console.ReadLine();
-            //string[] numberDayInputs = numbersDayInput.Split(',');
-
-            //Console.WriteLine();
-            // chooseDayList.Find(chooseDayList => chooseDayList.Id == numberDayInput).Code;
-            //string[] numberElementInputReplace = { numberElementInput,  };
+            var selectedFirstPartCodesList = new List<string>();
            
+            foreach (string val in numberDayInputs)
+            {
+                var linkFirstPart = chooseDayList.Find(x => x.Id == val);
+                selectedFirstPartCodesList.Add(linkFirstPart.Code);
+            }
+            return selectedFirstPartCodesList;
+
         }
-       
-        static void ChooseWeatherProperties()
+
+
+        //static List<string> GenerateFirstPartLink(List<string> SelectedFirstPartCodeList, List<string> SelectedDaysList)
+        //{
+        //    Console.WriteLine("\tWrite numbers which days do you want to download:");
+        //    var chooseDayList = new List<ChoosingProperties>
+        //    {
+        //         new ChoosingProperties("1", "cr/", "Today"),
+        //         new ChoosingProperties("2","crdl/","Tomorrow"),
+        //         new ChoosingProperties("3","crdl1/","Day After Tomorrow")
+        //    };
+
+        //    foreach (ChoosingProperties val in chooseDayList)
+        //    {
+        //        Console.WriteLine($"{val.Id} - {val.Name}");
+        //    }
+        //    Console.WriteLine("\te.g. 1,3");
+        //    string numbersDayInput = Console.ReadLine();
+        //    string[] numberDayInputs = numbersDayInput.Split(',');
+
+        //    var selectedFirstPartCodesList = new List<string>();
+        //    var selectedDaysList = new List<string>();
+
+        //    foreach (string val in numberDayInputs)
+        //    {
+        //        var linkFirstPart = chooseDayList.Find(x => x.Id == val);
+        //        selectedFirstPartCodesList.Add(linkFirstPart.Code);
+        //        selectedDaysList.Add(linkFirstPart.Name);
+
+        //    }
+        //    return selectedFirstPartCodesList, selectedDaysList;
+
+        //}
+
+
+
+
+        static List<string> GenerateSecondPartLink()
         {
             Console.WriteLine("\tWrite numbers which elements are you looking for");
-            //var weatherPropertiesList = new List<ChoosingProperties>() {
-            //    new ChoosingProperties(1,"oblcX","All Clouds"),
-            //    new ChoosingProperties(2,"srzk","Precipitation"),
-            //    new ChoosingProperties(3,"t2m","Temperature"),
-            //    new ChoosingProperties(4,"vitrx","Wind 10m"),
-            //    new ChoosingProperties(5,"vitry","Gust"),
-            //    new ChoosingProperties(6,"vitra","Wind 850 hPa"),
-            //    new ChoosingProperties(7,"vitrb","Wind 800 hPa"),
-            //    new ChoosingProperties(8,"drtr","Kind of Thermals"),
-            //    new ChoosingProperties(9,"cudf","Convective Temperature Deficit"),
-            //    new ChoosingProperties(10,"cukh","Cumulus Clouds"),
-            //    new ChoosingProperties(11,"cuvr","Climb Speed")
-            //};
-            //foreach (ChoosingProperties val in weatherPropertiesList)
-            //{
-            //    Console.WriteLine($"{val.Id} - {val.Name}");
-            //}
-            Console.WriteLine("e.g. 1,2,5,8");
+            var weatherPropertiesList = new List<ChoosingProperties> {
+                new ChoosingProperties("1","oblcX","All Clouds"),
+                new ChoosingProperties("2","srzk","Precipitation"),
+                new ChoosingProperties("3","t2m","Temperature"),
+                new ChoosingProperties("4","vitrx","Wind 10m"),
+                new ChoosingProperties("5","vitry","Gust"),
+                new ChoosingProperties("6","vitra","Wind 850 hPa"),
+                new ChoosingProperties("7","vitrb","Wind 800 hPa"),
+                new ChoosingProperties("8","drtr","Kind of Thermals"),
+                new ChoosingProperties("9","cudf","Convective Temperature Deficit"),
+                new ChoosingProperties("10","cukh","Cumulus Clouds"),
+                new ChoosingProperties("11","cuvr","Climb Speed")
+            };
+            foreach (ChoosingProperties val in weatherPropertiesList)
+            {
+                Console.WriteLine($"{val.Id} - {val.Name}");
+            }
+            Console.WriteLine("\te.g. 1,2,5,8");
 
             string numbersWeatherPropertiesInput = Console.ReadLine();
             string[] numberWeatherPropetiesInputs = numbersWeatherPropertiesInput.Split(',');
+
+            var selectedSecondPartCodesList = new List<string>();
+
+            foreach (string val in numberWeatherPropetiesInputs)
+            {
+                var linkSecondPart = weatherPropertiesList.Find(x => x.Id == val);
+                selectedSecondPartCodesList.Add(linkSecondPart.Code);
+
+            }
+            return selectedSecondPartCodesList;
         }
 
-        static void ChooseTime()
+        static List<string> GenerateThirdPartLink()
         {
 
             Console.WriteLine("\tWhat time are you looking for?");
@@ -197,38 +193,62 @@ namespace WeatherArchives
 
             string numbersTimeInput = Console.ReadLine();
             string[] numberTimeInputs = numbersTimeInput.Split(",");
-            
+
+            var selectedThirdPartCodesList = new List<string>();
+
+            foreach (string val in numberTimeInputs)
+            {
+                selectedThirdPartCodesList.Add(val);
+            }
+            return selectedThirdPartCodesList;
 
         }
         static void StartDownload()
         {
+            //foreach (string val in numberWeatherPropetiesInputs)                                     
+            //{
+            //    var linkSecondPart = weatherPropertiesList.Find(x => x.Id == val);
+            //    Console.WriteLine(linkSecondPart.Code);
+            //}
+            //WebClient webClient = new WebClient();
+            //webClient.DownloadFile(completeLink, fileName);
 
-          
         }
-        //public class Parachute
-        //{
-        //    public Parachute()
-        //    {
 
-        //    }
-        //    public Parachute(int hexColour)
-        //    {
-        //        HexColour = hexColour;
-        //    }
-        //    public int HexColour { get; set; } = 0;
-        //    public bool Open()
-        //    {
-        //        return false;
-        //    }
-        //    public bool Open(bool properPool)
-        //    {
-        //        return properPool;
-        //    }
-        //    public bool Open(string logo)
-        //    {
-        //        return false;
-        //    }
+        static void GenerateCompleteLinks()
+        {
+            string mainLink = "http://flymet.meteopress.cz/";
 
-        //}
+            foreach (string firstPartLink in firstPartLinks)
+            {
+                foreach (string secondPartLink in secondPartLinks)
+                {
+                    foreach (string thirdPartLink in thirdPartLinks)
+                    {
+                        string completeLink = $"{mainLink}{firstPartLink}{secondPartLink}{thirdPartLink}.png";
+                        Console.WriteLine(completeLink);
+                    }
+                }
+                    
+            }
+
+        }
+        static bool Confirmation()
+        {
+            Console.WriteLine("If you are sure click \"ENTER\"");
+
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+                return true;
+            else
+                return false;
+        }
+        static void GenerateAllProperties()
+        {
+            Console.WriteLine();
+        }
+        static void FileName()
+        {
+
+        }
     }
 }
