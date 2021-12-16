103da28eab66e047c820d3b6b707415cbb7d38d3
kamiljanek <gttony@o2.pl>
2021-12-16 01:42:00 +0100

    change type Int to String

--- WeatherArchives/WeatherArchives/ChoosingProperties.cs
+++ WeatherArchives/WeatherArchives/ChoosingProperties.cs
@@ -6,14 +6,14 @@ namespace WeatherArchives
 {
     public class ChoosingProperties
     {
-        //public ChoosingProperties(int id, string code, string name)
-        //{
-        //    Id = id;
-        //    Code = code;
-        //    Name = name;
-        //}
+        public ChoosingProperties(string id, string code, string name)
+        {
+            Id = id;
+            Code = code;
+            Name = name;
+        }
 
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
 
