480e4d5f7942b84037866464c41b393295eef0e4
kamiljanek <gttony@o2.pl>
2021-12-18 00:14:44 +0100

    new unused class

---
+++ WeatherArchives/WeatherArchives/NameFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherArchives
+{
+    public class NameFile : NameFolder
+    {
+        public string WeatherType { get; set; }
+    }
+}
