7673a1e69d57b645e1a9f49dab4e4bcbd9445e90
kamiljanek <gttony@o2.pl>
2021-12-18 00:14:58 +0100

    new used class

---
+++ WeatherArchives/WeatherArchives/NameFolder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherArchives
+{
+    public class NameFolder
+    {
+        public string Name { get; set; }
+    }
+}
