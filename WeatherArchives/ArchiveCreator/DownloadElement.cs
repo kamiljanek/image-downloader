4bd33d5edbfcdf9ca7d98950009aeaf52072f7a2
kamiljanek <gttony@o2.pl>
2022-06-28 22:43:31 +0200

    changed Elements to one property

--- WeatherArchives/ArchiveCreator/DownloadElement.cs
+++ WeatherArchives/ArchiveCreator/DownloadElement.cs
@@ -5,19 +5,19 @@ namespace ArchiveCreator
 {
     public class DownloadElement
     {
-        private Dictionary<string, string> _elements = new Dictionary<string, string>();
-        public Dictionary<string, string> Elements => _elements;
-
+        //private Dictionary<string, string> _elements = new Dictionary<string, string>();
+        //public Dictionary<string, string> Elements => _elements;
+        public Dictionary<string, string> Elements { get; private set; }
         public void AddElement(List<IGenerate> products)
         {
             foreach (var item in products)
             {
-                _elements.Add(item.GenerateUrl(), item.GenerateFileName());
+                Elements.Add(item.GenerateUrl(), item.GenerateFileName());
             }
         }
         public void AddElement(IGenerate product)
         {
-            _elements.Add(product.GenerateUrl(), product.GenerateFileName());
+            Elements.Add(product.GenerateUrl(), product.GenerateFileName());
         }
         
     }
