f04827119b8975d6867127390f169ae8dade99d2
kamiljanek <gttony@o2.pl>
2022-06-29 13:05:31 +0200

    back to previous property settings

--- WeatherArchives/ArchiveCreator/DownloadElement.cs
+++ WeatherArchives/ArchiveCreator/DownloadElement.cs
@@ -5,19 +5,18 @@ namespace ArchiveCreator
 {
     public class DownloadElement
     {
-        //private Dictionary<string, string> _elements = new Dictionary<string, string>();
-        //public Dictionary<string, string> Elements => _elements;
-        public Dictionary<string, string> Elements { get; private set; }
+        private Dictionary<string, string> _elements = new Dictionary<string, string>();
+        public Dictionary<string, string> Elements => _elements;
         public void AddElement(List<IGenerate> products)
         {
             foreach (var item in products)
             {
-                Elements.Add(item.GenerateUrl(), item.GenerateFileName());
+                _elements.Add(item.GenerateUrl(), item.GenerateFileName());
             }
         }
         public void AddElement(IGenerate product)
         {
-            Elements.Add(product.GenerateUrl(), product.GenerateFileName());
+            _elements.Add(product.GenerateUrl(), product.GenerateFileName());
         }
         
     }
