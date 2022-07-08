d660b7a6179400c904c6d1e04dc4c5277e0589bc
kamiljanek <gttony@o2.pl>
2022-07-08 19:15:16 +0200

    added FileOperation tests

--- WeatherArchives/Model.Tests/FileOperationTests.cs
+++ WeatherArchives/Model.Tests/FileOperationTests.cs
@@ -1,35 +1,38 @@
+using AutoFixture.Xunit2;
+using AutoFixture;
+using AutoFixture.AutoMoq;
 using Model;
+using Moq;
 using Xunit;
+using System.IO.Abstractions;
+using System.Net.Mail;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System.Security.Principal;
 
 namespace Model.Tests
 {
     public class FileOperationTests
     {
-        [Fact()]
-        public void FileReaderTest_ForReadFromJsonFile_ReturnsString()
+        [Fact]
+        public void FileReaderTest_ForReadFromJsonFile_ReturnsUserGmail()
         {
             //arrange
-            var fileOperation = new FileOperation();
-            var fileName = "fileOperationTest.json";
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var mock = fixture.Freeze<Mock<IFileSystem>>();
+            var sut = fixture.Create<FileOperation>();
+            var userGmail = fixture.Build<UserGmail>()
+                .With(x => x.GmailAddress, "asdfasf@gmail.com").Create();
+            var userGmailSerialised = JsonConvert.SerializeObject(userGmail);
+            var filePath = fixture.Create<string>();
+            mock.Setup(m => m.File.ReadAllText(filePath)).Returns(userGmailSerialised);
+
             //act
-            var message = fileOperation.FileReader<string>(fileName);
+            var expectedUserGmail = sut.FileReader<UserGmail>(filePath);
+            
             //assert
-            Assert.Equal("test message", message);
+            expectedUserGmail.Should().BeEquivalentTo(userGmail);
+            
         }
-
-        //[Fact()]
-        //public void FileGeneratorTest()
-        //{
-        //    //arrange
-
-        //    var fileOperation = new FileOperation();
-            
-        //    //act
-
-
-            
-        //    //assert
-        //    Assert.True(false, "This test needs an implementation");
-        //}
     }
 }
